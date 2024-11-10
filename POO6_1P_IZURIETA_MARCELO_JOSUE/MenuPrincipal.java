c37b67c5d6eb349884377a3f7af7e0534b4cc599
IZURIETA <domaizur@espol.edu.ec>
2024-11-09 22:33:00 -0500

    clasevehiculos y cehiculos residentes completadas/ opcion 6 terminada

--- POO6_1P_IZURIETA_MARCELO_JOSUE/MenuPrincipal.java
+++ POO6_1P_IZURIETA_MARCELO_JOSUE/MenuPrincipal.java
@@ -1,56 +1,76 @@
 import java.util.Scanner;
-public class MenuPrincipal{
-   
-    public static void main(String[] args){
+
+public class MenuPrincipal {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int eleccion;
         System.out.println("Bienvenido al menu principal de nuestro sistema \n Escoja las siguientes 8 opciones:");
 
-        do{
-        System.out.println("Opcion 1___Etapa");
-        System.out.println("Opcion 2___Residencia");
-        System.out.println("Opcion 3___Contactos por Residencia");
-        System.out.println("Opcion 4___Registro de Evento por Residencia ");
-        System.out.println("Opcion 5___Registro de Listado de Visitantes por Evento ");
-        System.out.println("Opcion 6___Registro de Ingreso o Salida de Vehículos de Residentes  ");
-        System.out.println("Opcion 7___Registro de Ingreso o Salida de Vehículos de Visitantes  ");
-        System.out.println("Opcion 8___Mostrar Listado de Vehículos por Residencia  ");
-        System.out.println("Opcion 9___Salir del sistema  ");
-        eleccion = sc.nextInt();
-        switch (eleccion){
-            case 1:
-            System.out.println("Ha elegido la opcion 1");
-            break;
-            case 2:
-            System.out.println("Ha elegido la opcion 2");
-            break;
-            case 3:
-            System.out.println("Ha elegido la opcion 3");
-            break;
-            case 4:
-            System.out.println("Ha elegido la opcion 4");
-            break;
-            case 5:
-            System.out.println("Ha elegido la opcion 5");
-            break;
-            case 6:
-            System.out.println("Ha elegido la opcion 6");
-            break;
-            case 7:
-            System.out.println("Ha elegido la opcion 7");
-            break;
-            case 8:
-            System.out.println("Ha elegido la opcion 8");
-            break;
-            case 9:
-            System.out.println("Saliendo...........\ntenga un buen dia");
-            break;
-            default:
-            System.out.println("Opcion invalida\nIntentelo nuevamente");
-            break;
-        }
-        System.out.println("");
-        }while (eleccion != 9);
-    sc.close();
- }
+        do {
+            System.out.println("Opcion 1___Etapa");
+            System.out.println("Opcion 2___Residencia");
+            System.out.println("Opcion 3___Contactos por Residencia");
+            System.out.println("Opcion 4___Registro de Evento por Residencia ");
+            System.out.println("Opcion 5___Registro de Listado de Visitantes por Evento ");
+            System.out.println("Opcion 6___Registro de Ingreso o Salida de Vehículos de Residentes  ");
+            System.out.println("Opcion 7___Registro de Ingreso o Salida de Vehículos de Visitantes  ");
+            System.out.println("Opcion 8___Mostrar Listado de Vehículos por Residencia  ");
+            System.out.println("Opcion 9___Salir del sistema  ");
+            eleccion = sc.nextInt();
+            switch (eleccion) {
+                case 1:
+                    System.out.println("Ha elegido la opcion 1");
+                    break;
+                case 2:
+                    System.out.println("Ha elegido la opcion 2");
+                    break;
+                case 3:
+                    System.out.println("Ha elegido la opcion 3");
+                    break;
+                case 4:
+                    System.out.println("Ha elegido la opcion 4");
+                    break;
+                case 5:
+                    System.out.println("Ha elegido la opcion 5");
+                    break;
+                case 6:
+                    System.out.println("Ha elegido la opcion 6");
+                    System.out.println(
+                            "Si va a registrar el ingreso de un vehiculo residente, escriba 1 \n Si va registrar la salida de un vehiculo residente escriba 2 \n Si desea volver al menu principal escriba 0 ");
+                    int opcion = sc.nextInt();
+                    do {
+                        switch (opcion) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                System.out.println("Por favor, intentelo nuevamente");
+                                break;
+                        }
+                        System.out.println("");
+
+                        break;
+                    } while (opcion != 0);
+
+                case 7:
+                    System.out.println("Ha elegido la opcion 7");
+                    break;
+                case 8:
+                    System.out.println("Ha elegido la opcion 8");
+                    break;
+                case 9:
+                    System.out.println("Saliendo...........\ntenga un buen dia");
+                    break;
+                default:
+                    System.out.println("Opcion invalida\nIntentelo nuevamente");
+                    break;
+            }
+            System.out.println("");
+        } while (eleccion != 9);
+        sc.close();
+    }
 }
--- POO6_1P_IZURIETA_MARCELO_JOSUE/Vehiculos_Residente.java
+++ POO6_1P_IZURIETA_MARCELO_JOSUE/Vehiculos_Residente.java
@@ -1,3 +1,102 @@
-public class VehiculosResidente extends Vehiculos{
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Vehiculos_Residente extends Vehiculos {
+    private Residente residente;
+    public static int n = 1;
     
-}
+
+    // * constructor del vehiculo residente */
+    public Vehiculos_Residente(Residente residente, String placa, String propietario, boolean estado) {
+        super(placa, propietario, estado);
+        this.residente = residente;
+    }
+
+    public Residente getResidente() {
+        return residente;
+    }
+
+    public void setResidente(Residente residente) {
+        this.residente = residente;
+
+    }
+
+    // *toString que muestra la información del vehiculo residente */
+    @Override
+    public String toString() {
+        return "Vehículo Residente [placa=" + getPlaca() + ", propietario=" + getPropietario() + ", estado="
+                + getEstado() +
+                ", residente=" + residente + "]";
+    }
+
+    // *muestra la informacion del propietario */
+    private void mostrarInformacionPropietario() {
+        System.out.println("Propietario del Vehículo: " + getPropietario());
+        System.out.println("Placa: " + getPlaca());
+        System.out.println("Residencia: " + residente.getNumeroResidencia());
+        // System.out.println("Ubicación: " + residente.getDireccionResidencia());
+    }
+
+    // *Registra el ingreso */
+    public void RegistarIngeso(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la placa del vehiculo:");
+        String placaIngresada =  sc.nextLine();
+        if (placaIngresada.equalsIgnoreCase(getPlaca())){ //!!!!!
+            if (!getEstado()){
+                setEstado(true);
+                System.out.println("Ingreso registrado para el vehículo: " + getPlaca());
+
+                mostrarInformacionPropietario();
+                guardarRegistro("ingreso");
+            }else{
+                System.out.println("El vehículo ya se encuentra dentro de la ciudadela.");
+
+            }else{
+                System.out.println("La placa ingresada no coincide con la del vehículo.");}
+
+        }
+    }
+
+    //*Registra la salida */
+    public void RegistarSalida(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la placa del vehículo: ");
+        String placaIngresada = sc.nextLine();
+        if (placaIngresada.equalsIgnoreCase(getPlaca())) {
+            if (getEstado()) {
+                setEstado(false); // Cambia el estado a fuera
+                System.out.println("Salida registrada para el vehículo con placa: " + getPlaca());
+                mostrarInformacionPropietario();
+                guardarRegistro("Salida");
+            } else {
+                System.out.println("El vehículo ya se encuentra fuera de la ciudadela.");
+            }
+        } else {
+            System.out.println("La placa ingresada no coincide con la del vehículo.");
+        }
+    }
+
+    }
+
+    
+
+    // *Crea el archivo si es la primera vez y guarda los ingreos */
+    private void guardarRegistro(String tipoMovimiento) {
+        try (FileWriter fw = new FileWriter("residentes.txt", true);
+                PrintWriter pw = new PrintWriter(fw)) {
+            pw.println("Acceso n :" + n);
+            pw.println("Movimiento: " + tipoMovimiento);
+            pw.println("Placa: " + getPlaca());
+            pw.println("Propietario: " + getPropietario());
+            pw.println("Residencia: " + residente.getNumeroResidencia());
+            pw.println("Estado actual: " + getEstado());
+            pw.println("--------------");
+            n += 1;
+        } catch (IOException e) {
+            System.out.println("Error al guardar el registro en residentes.txt: " + e.getMessage());
+        }
+    }
+
